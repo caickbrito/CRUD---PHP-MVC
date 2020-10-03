fd06d3f8c7cd5696aff8eba417d9fc8bf9160b62
Caick Brito <caickbr@gmail.com>
2020-10-03 11:00:19 -0300

    Ajuste em rota inicial

--- index.php
+++ index.php
@@ -10,14 +10,13 @@ $router->namespace('Source\App');
 
 //Rotas Principais// 
 $router->group(null);
+$router->get('/', 'Web:home', 'web.home');
 $router->get('/{page}', 'Web:home', 'web.home');
 $router->get('/edit/{id}', 'Web:edit', 'web.edit');
 $router->post('/edit', 'Web:editPost', 'web.editPost');
 $router->get('/delete/{id}', 'Web:delete', 'web.delete');
 $router->get("/register", "Web:register", "web.register");
 $router->post("/register", "Web:registerPost", "web.registerPost");
-
-
 
 //Rotas de Erros
 $router->group('erro');
